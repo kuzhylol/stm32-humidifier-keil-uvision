58f1b48c30daa84d189956c5afdf0ef721933391
jayawar <o.kuzhyl@gmail.com>
2020-09-05 00:12:24 +0300

    set size of clock cpu parameter

--- mcu_init.h
+++ mcu_init.h
@@ -1,8 +1,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define MCU_CLOCK_MHZ 48
-#define MCU_CLOCK_HZ MCU_CLOCK_MHZ * 1000000
+#define MCU_CLOCK_MHZ (uint32_t)48
+#define MCU_CLOCK_HZ MCU_CLOCK_MHZ * (uint32_t)1000000
 
 bool stm32f11re_init(void);
 void udelay(uint32_t us);
