2986ba945dce39844a40eefc10d915733ec63202
jayawar <o.kuzhyl@gmail.com>
2020-09-06 21:31:33 +0300

    wide timer demo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "hum_sensor.h"
 #include "piezo.h"
 
-extern volatile bool ut_flag, wt_flag;
+extern volatile bool wt_flag;
 
 int main(void) {
 	stm32f11re_init();
@@ -14,37 +14,34 @@ int main(void) {
 
 	Piezo piezo;
 	pz_init(&piezo);
-
-	uTimer utimer;
-	utim_init(&utimer, 10); // 1us * 10
 	
 	WideTimer wtimer;
 	wtim_init(&wtimer, 5);
 	
-	utimer.start();
+	HumSensor chip;
+	hs_init(&chip);
+
+	uTimer utimer;
+	utim_init(&utimer);
+
+	volatile unsigned int hum_temp[2] = {0};
+	volatile unsigned int hum = 0;
+	volatile unsigned int temp = 0;
+	int ret = 0;
 
 	wtimer.start();
 
-	HumSensor chip;
-	hs_init(&chip);
-	
-	udelay(100);
-
-	unsigned int hum_temp[2] = {0};
-	unsigned int hum = 0;
-	unsigned int temp = 0;
-	int ret = 0;
-	
 	while(1) {
 		if (true == wt_flag) {
-			piezo.toggle();
+			wtimer.stop();
 			ret = chip.hs_get_hum_temp(hum_temp);
-			if (ret > 0) {
+			if (ret) {
+				piezo.toggle();
 				hum = hum_temp[0];
 				temp = hum_temp[1];
 			}
-			
 			wt_flag = false;
+			wtimer.start();
 		}
 	}
 }
